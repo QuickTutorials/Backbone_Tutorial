7cecf898c92dfea3f9796eeb2ebed4fd961fd470
robersantiago <nisade@gmail.com>
2012-11-14 22:10:07 +0100

    Cambios para una comprensión más sencilla del código

--- part_4_routers/js/routers.js
+++ part_4_routers/js/routers.js
@@ -4,19 +4,22 @@ var AppRouter = Backbone.Router.extend({
             "*actions": "defaultRoute" // matches http://example.com/#anything-here
         }
 });
+
 // Initiate the router
 var app_router = new AppRouter;
 
-var mymodel=new TreeModel(data0);
+var myModel = new TreeModel(datos);
+
 app_router.on('route:paintTree', function(id) {
 	$("td").html("");
 	try{ 
-	//var mymodel=new TreeModel(data0);
-  	var treeView = new TreeView({el:$('#'+id),model:mymodel});
+  	var treeView = new TreeView({el:$('#'+id), model:myModel});
 	}catch(err){}
 })
+
 app_router.on('route:defaultRoute', function(actions) {
-	try{ alert("your route is: "+actions);
+	try{
+		alert("your route is: " + actions);
 	}catch(err){}
 })
 
--- part_4_routers/js/tree.js
+++ part_4_routers/js/tree.js
@@ -1,4 +1,4 @@
-var data0={ 
+var datos = { 
 		name:"Roclas",
 		email:"carlos@roclas.com",
 		items:{
@@ -22,37 +22,40 @@ var data0={
 		"Item 4":1
 		}
 };
-var TreeModel= Backbone.Model.extend({ defaults: { nombre:'-', email: 'adsfasdf@asfas.com' } });      
+
+var TreeModel = Backbone.Model.extend({defaults: {name: "Nombre", email: "Direccion email"}});      
 
 var TreeView = Backbone.View.extend({
-    //el: $('#tablaparticipantes'),
-    events: {
-      'click li.submenu': 'toogleFolder'
-    },
+  events: {
+    'click li.submenu': 'toogleFolder'
+  },
 
-    initialize: function(){
-      _.bindAll(this, 'render'); // remember: every function that uses 'this' as the current object should be in here
-      //this.model = new TreeModel(data0);
-      this.render();      
-    },
-    toogleFolder:function(i,e){
-	i.stopPropagation();
-	var myclass=i.srcElement.className;
-	var myselector="li."+myclass.replace(/ /g,".");
-	var display=$(myselector+">ul",this.el).css("display");
-	if(display=="none"){
-		$(myselector+">ul",this.el).css({"display":"block"});
-		$(myselector,this.el).attr("class", myclass.replace("closed","open"));
-	}else{
-		$(myselector+">ul",this.el).css({"display":"none"});
-		$(myselector,this.el).attr("class", myclass.replace("open","closed"));
-	}
-    },
-    template: _.template($("#template-tree").html()),
-    render: function() {
-    		var dict = this.model.toJSON();
-    		var html = this.template(dict);
-    		$(this.el).html(html);
-    }
+  initialize: function(){
+    _.bindAll(this, 'render');
+    
+    this.render();      
+  },
+
+  toogleFolder: function(i,e){
+		i.stopPropagation();
+		var myclass=i.srcElement.className;
+		var myselector="li."+myclass.replace(/ /g,".");
+		var display=$(myselector+">ul",this.el).css("display");
+		if(display=="none"){
+			$(myselector+">ul",this.el).css({"display":"block"});
+			$(myselector,this.el).attr("class", myclass.replace("closed","open"));
+		}else{
+			$(myselector+">ul",this.el).css({"display":"none"});
+			$(myselector,this.el).attr("class", myclass.replace("open","closed"));
+		}
+  },
+
+  template: _.template($("#template-tree").html()),
+
+  render: function() {
+  		var dict = this.model.toJSON();
+  		var html = this.template(dict);
+  		$(this.el).html(html);
+  }
 
 });
